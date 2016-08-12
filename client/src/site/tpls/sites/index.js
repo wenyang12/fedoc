232306264000bae5d9699166195fbfe216b0cb3b
张驰 <zhangc@firstshare.cn>
2016-08-12 15:49:59 +0800

    提交

--- client/src/site/tpls/sites/index.js
+++ client/src/site/tpls/sites/index.js
@@ -4,16 +4,33 @@ module.exports = function(app) {
         function($scope) {
             $scope.sites = [{
                 href: 'http://spark.firstshare.cn/',
-                name: '纷享前端组件spark'
+                name: '前端组件spark',
+                hot: true
+            }, {
+                href: 'http://registry-npm.firstshare.cn',
+                name: '私有npm',
+                hot: true
+            }, {
+                href: 'http://fe.firstshare.cn',
+                name: '文档平台'
             }, {
                 href: 'http://git.firstshare.cn/',
                 name: '基于gitlab代码仓库'
             }, {
-                href: 'http://registry-npm.firstshare.cn',
-                name: '纷享私有npm'
+                href: 'http://172.31.101.23:9080/',
+                name: 'sde,fte,fte2等环境前端部署'
             }, {
-                href: 'http://fe.firstshare.cn',
-                name: '纷享文档'
+                href: 'http://open.fsfte2.com/open/jsapi/doc/',
+                name: 'jsapi文档'
+            }, {
+                href: 'http://vwsr100014:8080/',
+                name: '安卓打包地址'
+            }, {
+                href: 'http://172.29.0.169:8080/',
+                name: 'IOS打包地址'
+            }, {
+                href: 'http://172.31.101.16:12000/signin.html',
+                name: '微营销h5模板制作'
             }];
         }
     ]).config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
