940a886acafe52c9254a28ffff14930c01e586d6
张驰 <zhangc@firstshare.cn>
2016-09-01 11:37:47 +0800

    取消完成构建响应

--- server/controller/deploy.js
+++ server/controller/deploy.js
@@ -12,9 +12,7 @@ var mongoose = require('mongoose'),
 
 exports.init = function(req, res) {
     var startSt = new Date().getTime();
-    console.log('[构建开始]' + dirPath.root);
     var cmd = spawn(dirPath.root + '/deploy.sh');
-
     cmd.stdout.on('data', function(data) {
         console.log('stdout: ' + data.toString());
     });
@@ -24,9 +22,8 @@ exports.init = function(req, res) {
     });
 
     cmd.on('exit', function(code) {
-        var log = '[构建成功] 耗时：' + (new Date().getTime() - startSt) + 'ms';
-        console.log(log);
-        res.send(log);
+
     });
+    res.send('[构建开始]' + dirPath.root);
 
 };
